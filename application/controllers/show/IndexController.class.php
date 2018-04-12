7bca757a2fd1e2befea8f2e1156ba5c51b98f906
kjkwer <lilinfeng1024@163.com>
2018-04-12 10:37:35 +0800

    版本更新1.1

--- application/controllers/show/IndexController.class.php
+++ application/controllers/show/IndexController.class.php
@@ -12,6 +12,7 @@ class IndexController extends BaseController
         //>>获取用户数据
         $userId  = !empty($_COOKIE["user"])?$_COOKIE["user"]:"";
         $model = new ModelNew("photo");
+        $chaeck = "";
         $data = $model->where(["user"=>$userId])->find()->one();
         include CUR_VIEW_PATH."Sindex" . DS . "index_index.html";
     }
@@ -20,6 +21,7 @@ class IndexController extends BaseController
         $result = array("code"=>500,"message"=>"文件上传失败");
         //>>图片数据
         $pictureData = $_FILES['fileToUpload'];
+        $pictureSize = $pictureData["size"]/1024;
         //>>判断文件上传是否正常
         if($pictureData["error"]==0){
             //>>判断文件上传格式是否正确
@@ -33,6 +35,11 @@ class IndexController extends BaseController
                 //>>移动文件
                 $file = $filePath."/".uniqid().stristr($pictureData['name'],'.');
                 if(move_uploaded_file($pictureData["tmp_name"],$file)){
+                    if ($pictureSize>=500){
+                        $this->library("Imgcompress");
+                        $image  = new Imgcompress($file,1);
+                        $image->compressImg($file);
+                    }
                     $result = array("code"=>200,"message"=>$file);
                 }else{
                     $result = array("code"=>502,"message"=>"文件保存失败");
@@ -43,6 +50,13 @@ class IndexController extends BaseController
         }
         echo json_encode($result);
     }
+    public function shareyAction(){
+        $id = !empty($_GET["id"])?$_GET["id"]:"";
+        $model = new ModelNew("photo");
+        $data = $model->where(["id"=>$id])->find()->one();
+        $chaeck = 89;
+        include CUR_VIEW_PATH."Sindex" . DS . "index_share.html";
+    }
     //>>保存数据
     public function saveAction(){
         $result = array("code"=>500,"message"=>"相册保存失败");
@@ -50,20 +64,21 @@ class IndexController extends BaseController
         $data = $model->where(["user"=>$_POST["user"]])->find("id")->one();
         $id = !empty($data["id"])?$data["id"]:'';
         if ($id == ""){
-            if ($model->insert($_POST)){
-                $result = array("code"=>200,"message"=>"相册保存成功");
+            if ($rs = $model->insert($_POST)){
+                $result = array("code"=>200,"message"=>"相册保存成功","id"=>$rs);
             }else{
-                $result = array("code"=>501,"message"=>"相册保存失败");
+                $result = array("code"=>501,"message"=>"相册保存失败","id"=>"");
             }
         }else{
             unset($_POST["id"]);
+            $_POST["dtime"] = date("Y-m-d H:i:s",time());
             if($model->where(["id"=>$id])->update($_POST)){
-                $result = array("code"=>200,"message"=>"相册保存成功");
+                $result = array("code"=>200,"message"=>"相册保存成功","id"=>$id);
             }else{
-                $result = array("code"=>502,"message"=>"相册保存失败");
+                $result = array("code"=>502,"message"=>"相册保存失败","id"=>"");
             }
         }
-        setcookie("user",$_POST["user"],time()+365*24*3600);
+        setcookie("user",$_POST["user"],time()+3600*24*365);
         echo json_encode($result);
     }
     //>>设置cookie模拟登陆
@@ -72,5 +87,16 @@ class IndexController extends BaseController
     }
     public function getAction(){
         echo $_COOKIE["id"];
+    }
+    public function shareAction(){
+        //微信分享
+        $this->library("JSSDKoth");
+        $appid = "wxc3c007cddc714a5d";
+        $appsecret = "b3e7d6edeec7dcbb2255179b3b634f2a";
+        $jssdk = new JSSDK($appid,$appsecret);
+        $jssdk->url = $_GET['url'];
+        $data['package'] = $jssdk->GetSignPackage();
+        $data['status'] = true;
+        $this->ajaxReturn($data);
     }
 }
--- public/photoAlbum/js/test.js
+++ public/photoAlbum/js/test.js
@@ -10,21 +10,18 @@ $(function () {
         afterLoad: aa,
         onLeave: bb,
     });
-
     function bb(a, b, c) {
         if(b==10){
             $('.jiantou').removeClass('updown');
         }else{
             let ddd = $('.jiantou').hasClass('updown');
-            console.log(ddd)
             if(!ddd){
                 $('.jiantou').addClass('updown')
             }
         }
         zhixing(b);
-        
     }
-
+    
     function aa(a, b) {
         console.log(a, b)
     }
@@ -41,6 +38,71 @@ $(function () {
             music.pause();
             $('.music').removeClass('xuanzhuan');
         }
+    })
+
+
+    // document.onclick = function(){
+    //     var c = $('#menu li').index($('#menu li.active'))
+    //     c = parseInt(c) + 1;
+    //     $('#dowebok').fullpage.moveTo(c);
+    // }
+    // var ddd = 0;
+    // $('input').blur(function(){
+    //     gogo();
+    // })
+    // $('textarea').blur(function(){
+    //     gogo();
+    // })
+    // $('input').focus(function(){
+    //     let c = $('#dowebok').css('transform');
+    //     c = c.split(',');
+    //     c = c[(c.length-1)];
+    //     c = c.replace(')','');
+    //     c = parseInt(c);
+    //     ddd = c;
+    //     console.log(c)
+    // })
+    // function gogo(){
+    //     $('#dowebok').css('transform','translate3d(0px,'+ddd+'px,0px)');
+    // }
+
+    var target = null;
+    $('.Shuru').click(function(){
+        $('.tanchu input').show();
+        $('.tanchu textarea').hide();
+        $('.zhezhao').show();
+        $('.tanchu').show();
+        target = $(this);
+        // $('.tanchu input').val(target.text());
+    })
+    $('.s3_s3').click(function(){
+        $('.tanchu input').hide();
+        $('.tanchu textarea').show();
+        $('.zhezhao').show();
+        $('.tanchu').show();
+        target = $(this);
+    })
+    $('.zhezhao').click(function(){
+        $('.zhezhao').hide();
+        $('.tanchu').hide();
+        $('.tanchu input').val('');
+        $('.tanchu textarea').val('');
+    })
+
+    $('.tanchu .but1').click(function(){
+        var c = $('.tanchu input').val();
+        var d = $('.tanchu textarea').val();
+        if(c!=''){
+            target.text(c);
+        }else{
+            d = d.replace(/[\n\r]/g,'<br>');
+            target.html(d);
+        }
+        $('.zhezhao').hide();
+        $('.tanchu').hide();
+        $('.tanchu input').val('');
+        $('.tanchu textarea').val('');
+
     })
 })
 
